ce75970813c9d747aa5ac096b9577101e79160ed
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2023-01-25 15:36:17 +0530

    dynamic load

--- src/components/editAruco.jsx
+++ src/components/editAruco.jsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
+import rem2 from './../../public/images/authImage.png';
+import rem3 from './../../public/images/removeBlue.png';
 
-export const EditAruco = () => {
+export default function EditAruco() {
   return (
     <div>
       <Image src={rem2} alt='test1' width={200} height={200} unoptimized />
       <Image src={rem3} alt='test1' width={200} height={200} unoptimized />
     </div>
   );
-};
+}
